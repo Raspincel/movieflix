9c9b7f32a7c1c8c7f3ae17f92605259986f6d448
Ian Silva <raspincel@gmail.com>
2025-04-02 09:03:26 -0300

    fix: use icon from lucide instead of pasted svg

--- src/components/search/Input.tsx
+++ src/components/search/Input.tsx
@@ -45,18 +45,7 @@ const SearchInput = ({ isExpanded, onFocus, searchQuery, onClose, onChange, isLo
   return (
     <div className="relative">
       <div className="absolute inset-y-0 left-3 flex items-center pointer-events-none">
-        <svg
-          className="w-5 h-5 text-red-500"
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 20 20"
-          fill="currentColor"
-        >
-          <path
-            fillRule="evenodd"
-            d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
-            clipRule="evenodd"
-          />
-        </svg>
+        <Search className="w-5 h-5 text-red-500" />
       </div>
 
       {isLoading ? (
